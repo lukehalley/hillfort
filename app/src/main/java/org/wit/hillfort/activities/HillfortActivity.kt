b6d59babe4e48aa963fffebec007d2d5370f73ff
LHK <lhk@LHKs-MacBook-Pro.local>
2018-10-16 11:48:20 +0100

    Small fix to not exit the hillfort add/edit pane if the user does not enter the description/title fields

--- app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortActivity.kt
@@ -20,90 +20,90 @@ import org.wit.hillfort.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
-  var hillfort = HillfortModel()
-  lateinit var app: MainApp
-  val IMAGE_REQUEST = 1
-  val LOCATION_REQUEST = 2
-  var location = Location(52.245696, -7.139102, 15f)
+    var hillfort = HillfortModel()
+    lateinit var app: MainApp
+    val IMAGE_REQUEST = 1
+    val LOCATION_REQUEST = 2
+    var location = Location(52.245696, -7.139102, 15f)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_hillfort)
-    toolbarAdd.title = title
-    setSupportActionBar(toolbarAdd)
-    info("Hillfort Activity started..")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hillfort)
+        toolbarAdd.title = title
+        setSupportActionBar(toolbarAdd)
+        info("Hillfort Activity started..")
 
-    app = application as MainApp
-    var edit = false
+        app = application as MainApp
+        var edit = false
 
-    if (intent.hasExtra("hillfort_edit")) {
-      edit = true
-      hillfort = intent.extras.getParcelable<HillfortModel>("hillfort_edit")
-      hillfortTitle.setText(hillfort.title)
-      description.setText(hillfort.description)
-      hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
-      if (hillfort.image != null) {
-        chooseImage.setText(R.string.change_hillfortImage)
-      }
-      btnAdd.setText(R.string.button_saveHillfort)
-    }
-
-    btnAdd.setOnClickListener() {
-      hillfort.title = hillfortTitle.text.toString()
-      hillfort.description = description.text.toString()
-      if (hillfort.title.isEmpty()) {
-        toast(R.string.hint_EnterHillfortTitle)
-      } else {
-        if (edit) {
-          app.hillforts.update(hillfort.copy())
-        } else {
-          app.hillforts.create(hillfort.copy())
+        if (intent.hasExtra("hillfort_edit")) {
+            edit = true
+            hillfort = intent.extras.getParcelable<HillfortModel>("hillfort_edit")
+            hillfortTitle.setText(hillfort.title)
+            description.setText(hillfort.description)
+            hillfortImage.setImageBitmap(readImageFromPath(this, hillfort.image))
+            if (hillfort.image != null) {
+                chooseImage.setText(R.string.change_hillfortImage)
+            }
+            btnAdd.setText(R.string.button_saveHillfort)
         }
-      }
-      info("add Button Pressed: $hillfortTitle")
-      setResult(AppCompatActivity.RESULT_OK)
-      finish()
-    }
 
-    chooseImage.setOnClickListener {
-        showImagePicker(this, IMAGE_REQUEST)
-    }
-
-    hillfortLocation.setOnClickListener {
-      startActivityForResult(intentFor<MapsActivity>().putExtra("location", location), LOCATION_REQUEST)
-    }
-  }
-
-  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-    menuInflater.inflate(R.menu.menu_hillfort, menu)
-    return super.onCreateOptionsMenu(menu)
-  }
-
-  override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-    when (item?.itemId) {
-      R.id.item_cancel -> {
-        finish()
-      }
-    }
-    return super.onOptionsItemSelected(item)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    when (requestCode) {
-      IMAGE_REQUEST -> {
-        if (data != null) {
-          hillfort.image = data.getData().toString()
-          hillfortImage.setImageBitmap(readImage(this, resultCode, data))
-          chooseImage.setText(R.string.change_hillfortImage)
+        btnAdd.setOnClickListener() {
+            hillfort.title = hillfortTitle.text.toString()
+            hillfort.description = description.text.toString()
+            if (hillfort.title.isEmpty() or hillfort.description.isEmpty()) {
+                toast(R.string.hint_EnterHillfortTitle)
+            } else {
+                if (edit) {
+                    app.hillforts.update(hillfort.copy())
+                } else {
+                    app.hillforts.create(hillfort.copy())
+                }
+                info("add Button Pressed: $hillfortTitle")
+                setResult(AppCompatActivity.RESULT_OK)
+                finish()
+            }
         }
-      }
-      LOCATION_REQUEST -> {
-        if (data != null) {
-          location = data.extras.getParcelable<Location>("location")
+
+        chooseImage.setOnClickListener {
+            showImagePicker(this, IMAGE_REQUEST)
         }
-      }
+
+        hillfortLocation.setOnClickListener {
+            startActivityForResult(intentFor<MapsActivity>().putExtra("location", location), LOCATION_REQUEST)
+        }
     }
-  }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_hillfort, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.item_cancel -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    hillfort.image = data.getData().toString()
+                    hillfortImage.setImageBitmap(readImage(this, resultCode, data))
+                    chooseImage.setText(R.string.change_hillfortImage)
+                }
+            }
+            LOCATION_REQUEST -> {
+                if (data != null) {
+                    location = data.extras.getParcelable<Location>("location")
+                }
+            }
+        }
+    }
 }
 
