35e443f626f9800663cb1de5e9ab9e5c1c8d4d05
lukehalley <luke123halley@gmail.com>
2018-10-19 10:10:27 +0100

    Update now working

--- app/src/main/java/org/wit/hillfort/models/HillfortJSONStore.kt
+++ app/src/main/java/org/wit/hillfort/models/HillfortJSONStore.kt
@@ -15,43 +15,49 @@ val gsonBuilder = GsonBuilder().setPrettyPrinting().create()
 val listType = object : TypeToken<java.util.ArrayList<HillfortModel>>() {}.type
 
 fun generateRandomId(): Long {
-  return Random().nextLong()
+    return Random().nextLong()
 }
 
 class HillfortJSONStore : HillfortStore, AnkoLogger {
 
-  val context: Context
-  var hillforts = mutableListOf<HillfortModel>()
+    val context: Context
+    var hillforts = mutableListOf<HillfortModel>()
 
-  constructor (context: Context) {
-    this.context = context
-    if (exists(context, JSON_FILE)) {
-      deserialize()
+    constructor (context: Context) {
+        this.context = context
+        if (exists(context, JSON_FILE)) {
+            deserialize()
+        }
     }
-  }
 
-  override fun findAll(): MutableList<HillfortModel> {
-    return hillforts
-  }
+    override fun findAll(): MutableList<HillfortModel> {
+        return hillforts
+    }
 
-  override fun create(hillfort: HillfortModel) {
-    hillfort.id = generateRandomId()
-    hillforts.add(hillfort)
-    serialize()
-  }
+    override fun create(hillfort: HillfortModel) {
+        hillfort.id = generateRandomId()
+        hillforts.add(hillfort)
+        serialize()
+    }
 
 
-  override fun update(hillfort: HillfortModel) {
-    // todo
-  }
+    override fun update(hillfort: HillfortModel) {
+        var foundHillfort: HillfortModel? = hillforts.find { p -> p.id == hillfort.id }
+        if (foundHillfort != null) {
+            foundHillfort.title = hillfort.title
+            foundHillfort.description = hillfort.description
+            foundHillfort.image = hillfort.image
+            serialize()
+        }
+    }
 
-  private fun serialize() {
-    val jsonString = gsonBuilder.toJson(hillforts, listType)
-    write(context, JSON_FILE, jsonString)
-  }
+    private fun serialize() {
+        val jsonString = gsonBuilder.toJson(hillforts, listType)
+        write(context, JSON_FILE, jsonString)
+    }
 
-  private fun deserialize() {
-    val jsonString = read(context, JSON_FILE)
-    hillforts = Gson().fromJson(jsonString, listType)
-  }
+    private fun deserialize() {
+        val jsonString = read(context, JSON_FILE)
+        hillforts = Gson().fromJson(jsonString, listType)
+    }
 }
