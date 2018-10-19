bb9450ab18aaf45cdbb9f2599bd8f6c1f8f0e650
lukehalley <luke123halley@gmail.com>
2018-10-19 16:55:25 +0100

    Mirrored Hillfort save code for user register

---
+++ app/src/main/java/org/wit/hillfort/models/UserModel.kt
@@ -0,0 +1,16 @@
+package org.wit.hillfort.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class HillfortModel(var id: Long = 0,
+                          var title: String = "",
+                          var description: String = "",
+                          var image: String = "") : Parcelable
+
+@Parcelize
+data class Location(var lat: Double = 0.0,
+                    var lng: Double = 0.0,
+                    var zoom: Float = 0f) : Parcelable
+
