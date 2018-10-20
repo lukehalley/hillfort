f712998a1ec624751903c80568c053360a941ec4
lukehalley <luke123halley@gmail.com>
2018-10-20 14:25:37 +0100

    Users can now log in

--- app/src/main/java/org/wit/hillfort/activities/HillfortLoginActivity.kt
+++ app/src/main/java/org/wit/hillfort/activities/HillfortLoginActivity.kt
@@ -8,16 +8,21 @@ import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivityForResult
 import org.jetbrains.anko.toast
 import org.wit.hillfort.R
+import org.wit.hillfort.main.MainApp
 
 class HillfortLoginActivity : AppCompatActivity(), AnkoLogger {
 
+    lateinit var app: MainApp
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        app = application as MainApp
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         loginButton.setOnClickListener {
-            // Checking if the user enters the correct log in details
-            if (enteredEmail.text.toString() == ("luke123halley@gmail.com") && enteredPassword.text.toString() == ("password")) {
+            var users = app.users.findAll()
+            if (enteredEmail.text.toString() in users.toString() && enteredPassword.text.toString() in users.toString()) {
                 info { "Logged In!" }
                 startActivityForResult<HillfortListActivity>(0)
             } else {
