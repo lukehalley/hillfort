07f2b9ed90063d6ab53a969da0330bd83a12b600
lukehalley <luke123halley@gmail.com>
2018-10-27 16:31:54 +0100

    Trying to make sure user wants to exit on up pressed but cant get to work

---
+++ Hillfort/src/main/java/org/wit/hillfort/models/HillfortModel.kt
@@ -0,0 +1,25 @@
+package org.wit.hillfort.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class HillfortModel(var id: Long = 0,
+                         var title: String = "",
+                         var description: String = "",
+                         var addNotes: String = "",
+                         var visited: Boolean = false,
+                         var dateVisited: String = "",
+                         var lat : Double = 0.0,
+                         var lng: Double = 0.0,
+                         var zoom: Float = 0f,
+                         var firstImage: String = "",
+                         var secondImage: String = "",
+                         var thirdImage: String = "",
+                         var fourthImage: String = "") : Parcelable
+
+@Parcelize
+data class Location(var lat: Double = 0.0,
+                    var lng: Double = 0.0,
+                    var zoom: Float = 0f) : Parcelable
+
--- app/src/main/java/org/wit/hillfort/models/HillfortStore.kt
+++
@@ -1,8 +0,0 @@
-package org.wit.hillfort.models
-
-interface HillfortStore {
-    fun findAll(): List<HillfortModel>
-    fun create(hillfort: HillfortModel)
-    fun update(hillfort: HillfortModel)
-    fun delete(hillfort: HillfortModel)
-}
