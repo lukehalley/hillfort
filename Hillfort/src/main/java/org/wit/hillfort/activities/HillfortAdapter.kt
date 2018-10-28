29cdf8f1272cc4b8f48b574c41045e7de292365c
lukehalley <luke123halley@gmail.com>
2018-10-28 14:04:50 +0000

    Visited indicator now implemented

--- Hillfort/src/main/java/org/wit/hillfort/activities/HillfortAdapter.kt
+++ Hillfort/src/main/java/org/wit/hillfort/activities/HillfortAdapter.kt
@@ -1,4 +1,5 @@
 package org.wit.hillfort.activities
+import android.graphics.Color
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -39,6 +40,15 @@ class HillfortAdapter(private var hillforts: List<HillfortModel>,
             itemView.cardHillfortLocation.text = "Address: " + hillfort.address
             itemView.imageIcon.setImageBitmap(readImageFromPath(itemView.context, hillfort.firstImage))
             itemView.setOnClickListener { listener.onHillfortClick(hillfort) }
+
+            if (hillfort.visited){
+                itemView.visitedIndicator.setBackgroundColor(Color.parseColor("#5db761"))
+                itemView.visitedIndicator.setText(R.string.isVisited)
+            } else {
+                itemView.visitedIndicator.setBackgroundColor(Color.parseColor("#FF9E9E9E"))
+                itemView.visitedIndicator.setText(R.string.notVisited)
+            }
+
         }
     }
 }
